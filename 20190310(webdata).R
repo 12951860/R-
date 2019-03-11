f28712a6df2e5582bdd102012148d6afb3709c6a
12951860 <skbm653@gmail.com>
2019-03-12 00:49:30 +0900

    20190311JSON까지

--- 20190310(webdata).R
+++ 20190310(webdata).R
@@ -6,3 +6,21 @@ install.packages('readr')
 #encoding방식으로는 euc-kr을 입력후 OK누르고 Import로 데이터를 가져옴
 #데이터를 가져올 때엔 데이터가 변형되거나 누락된 부분이 있는지 확인할 것
 
+#data.seoul.go.kr에서 공기업리스트 > 한국어 > 인증키신청
+install.packages('XML')
+library(XML)
+url <- 'http://openapi.seoul.go.kr:8088/6764526474776f6734377355794252/xml/SebcPublicEnterpriseKor/1/5/'
+indata <- xmlToDataFrame(url)  
+View(indata)
+indata <- indata[-1:-2,-1:-3]
+
+
+#JSON데이터
+install.packages('jsonlite')
+library(jsonlite)
+install.packages('curl')
+library(curl)
+data2 <- fromJSON("https://api.github.com/users/hadley/orgs")
+data2 <- as.data.frame(data2)
+colnames(data2)
+View(data2)
