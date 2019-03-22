013a87f16a7fc52aee8cbb87499773d04d97bb31
12951860 <skbm653@gmail.com>
2019-03-23 01:43:25 +0900

    20190323회귀분석부분포함

--- 20190319.R
+++ 20190319.R
@@ -7,3 +7,24 @@ summary(as.factor(NHIS_OPEN_GJ_2015$성별코드))
 
 library(XML)
 library(dplyr)
+#체중데이터 가져오기
+data <- NHIS_OPEN_GJ_2015 %>%
+   select('신장(5Cm단위)','체중(5Kg 단위)')
+head(data)
+#상관관계알아보기
+cor(data, method = "pearson")
+cor(data$`신장(5Cm단위)`,data$`체중(5Kg 단위)`,method="pearson")
+# H0: 신장과 체중은 관계가 없다.
+cor.test(data$`신장(5Cm단위)`,data$`체중(5Kg 단위)`)
+#관계가 있어보인다.(양의 상관관계)
+
+#회귀분석
+data2 <- NHIS_OPEN_GJ_2015 %>%
+  select('체중(5Kg 단위)',허리둘레,'신장(5Cm단위)')
+set.seed(123)
+idx <- sample(2,size=NROW(data2),replace=T,prob=c(0.7,0.3))
+train_data <- data2[idx==1,]
+test_data <- data2[idx==2,]
+train_result <- lm(train_data$`체중(5Kg 단위)`~ train_data$허리둘레 + train_data$`신장(5Cm단위)`)
+summary(train_result)
+
